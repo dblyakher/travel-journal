6b61ced74df61154ab0906ddf7d09bc77f6a230e
dblyakher <101061863+dblyakher@users.noreply.github.com>
2022-10-20 21:33:02 -0400

    Add files via upload

---
+++ Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="../Images/Globe.svg" className="header--img"/>
+            <h3 className="header--title">my travel journal.</h3>
+        </div>
+    )
+}
+
+export default Header
